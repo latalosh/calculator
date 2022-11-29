6bb9c40962e30fea1961511fc5484a66604d2881
lalosh <louayalosh17@gmail.com>
2022-11-29 16:28:00 +0200

    Add simple documentation for calculator functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const calculator = {
 
+  /*
+   * take two numbers, and return the sum of them
+   */
   sum(x,y){
     return x + y;
   },
 
+
+  /*
+   * take two numbers, and return the subtraction of them
+   */
   subtract(x,y){
     return x - y;
   },
