758a4b29e61b0546128a1ffa0b996bd1ab4c0596
lalosh <louayalosh17@gmail.com>
2022-11-29 17:19:44 +0200

    Feat: update calculator UI Logic

--- src/index.js
+++ src/index.js
@@ -1,54 +1,88 @@
 document.addEventListener('DOMContentLoaded', startApp);
 
-let firstNumber = '';
-let secondNumber = '';
-let operation = '';
-
 
 function startApp() {
 
+  let firstNumberElement = document.querySelector('#first-number');
+  let secondNumberElement = document.querySelector('#second-number');
+  let operationElement = document.querySelector('#operation');
+  let resultElement = document.querySelector('#result');
+
+  let firstNumber = '';
+  let secondNumber = '';
+  let operation = '';
+  let lock = false;
+
   let buttonsContainer = document.querySelector('.buttons-container');
 
-  buttonsContainer.addEventListener('click', function eventHandler(event) {
+  buttonsContainer.addEventListener('click', calculatorButtonClickHandler);
+
+
+  function resetAll() {
+    lock = false;
+    firstNumber = '';
+    secondNumber = '';
+    operation = '';
+    firstNumberElement.innerText = '';
+    secondNumberElement.innerText = '';
+    operationElement.innerText = '';
+    resultElement.innerText = '';
+  }
+
+
+
+  function calculatorButtonClickHandler(event) {
     let selectedButton = event.target.innerText;
 
     let isNumber = /\d/g.test(selectedButton);
     let isOperation = /\+|\x|\-|\//.test(selectedButton);
     let isEqual = /=/g.test(selectedButton);
+    let isAC = /AC/.test(selectedButton);
+
+    if (isAC)
+      resetAll();
+
+    if (lock)
+      return;
 
     /*
     * only save the operation if 
     * - first number exist 
     * - no operation is saved before
     */
-    if (isOperation && firstNumber && !operation)
+    if (isOperation && firstNumber && !operation) {
       operation = selectedButton;
+      operationElement.innerText = operation;
+    }
 
 
     if (isNumber)
-      if (!firstNumber)
+      if (!firstNumber || !operation) {
         firstNumber += selectedButton;
-      else
+        firstNumberElement.innerText = firstNumber;
+      }
+      else {
         secondNumber += selectedButton;
+        secondNumberElement.innerText = secondNumber;
+      }
 
 
     if (isEqual) {
-      let result = calculateTheResult();
-      console.log({ result });
-      firstNumber = '';
-      secondNumber = '';
-      operation = '';
+      lock = true;
+      let result = calculateTheResult({ firstNumber, secondNumber, operation });
+      resultElement.innerText = ` = ${result}`;
     }
+  }
 
-    console.log({ firstNumber, secondNumber, operation });
-  });
 
 }
 
-function calculateTheResult() {
+function calculateTheResult({ firstNumber, secondNumber, operation }) {
   switch (operation) {
     case '+': {
       return calculator.sum(firstNumber, secondNumber);
     }
+    default:
+      return '';
   }
 }
